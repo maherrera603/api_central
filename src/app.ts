cecf414f506f3cb61eaf52e86ffdc795747e1c12
maherrera603 <maherrera603@misena.edu.co>
2025-01-07 15:37:43 -0500

    implementation docker-compose and connection to the database

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { envs } from "./config";
+import { MongoConnect } from "./data";
 import { Server } from "./presentation"
 import { AppRouter } from "./presentation/routes";
 
@@ -6,7 +7,12 @@ import { AppRouter } from "./presentation/routes";
     main()
 })()
 
-function main(){
+async function main(){
+
+    await MongoConnect.connect({
+        mongoUrl: envs.MONGO_URL,
+        dbName: envs.DB_NAME
+    });
 
     const server = new Server({
         port: envs.PORT,
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,5 +3,7 @@ import { get } from "env-var";
 
 
 export const envs = {
-    PORT: get("PORT").required().asPortNumber()
+    PORT: get("PORT").required().asPortNumber(),
+    MONGO_URL: get("MONGO_URL").required().asString(),
+    DB_NAME: get("DB_NAME").required().asString()
 }
